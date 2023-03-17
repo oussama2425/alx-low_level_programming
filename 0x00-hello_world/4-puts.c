cf85902730890ead2cd4f1098b75481645c55930
oussama <oussama1mellouli@gmail.com>
2023-03-17 09:27:02 -0700

    name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+*main - entry poit
+*return : always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
