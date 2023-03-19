3ed8f29d7d7a6edf8dfdfbfb4b1b85c85de747e2
oussama <oussama1mellouli@gmail.com>
2023-03-19 09:01:28 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Write a C program that prints exactly and that piece of art is useful
+ * main - A c programm
  * Return 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
